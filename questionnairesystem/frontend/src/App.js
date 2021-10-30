b1d7fce08eb991bc6ef4f5dc2464a2516f135c95
justinhoejj <hoejj05@gmail.com>
2021-10-30 23:57:45 +0800

    Refactor homepage

--- questionnairesystem/frontend/src/App.js
+++ questionnairesystem/frontend/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/layout/Header'
-import Dashboard from './components/Dashboard'
-import CreateResponsePage from './pages/CreateResponsePage'
 import { Provider } from 'react-redux'
 import store from './store'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import HomePage from './pages/HomePage'
 import EditRersponsePage from './pages/EditRersponsePage'
+import CreateResponsePage from './pages/CreateResponsePage'
+
 
 const App = () => (
     <Provider store={store}>
       <Header />
       <Router>
         <Switch>
-          <Route exact path='/' component={Dashboard} />
+          <Route exact path='/' component={HomePage} />
           <Route exact path='/create' component={CreateResponsePage} />
           <Route exact path='/edit' component={EditRersponsePage} />
         </Switch>
---
+++ questionnairesystem/frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Responses from '../components/Responses'
+
+function HomePage() {
+  return (
+    <div>
+      <Responses/>
+    </div>
+  )
+}
+
+export default HomePage

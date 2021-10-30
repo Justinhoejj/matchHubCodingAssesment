af7523d949d4e1678f043d5ecfbc017645f7e8a0
justinhoejj <hoejj05@gmail.com>
2021-10-30 15:18:06 +0800

    Refactor app

---
+++ questionnairesystem/frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div className="App">
+    <h1>Heloo</h1>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'));
